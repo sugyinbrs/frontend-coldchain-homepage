1d99aa682dab2bc4cebf7a89f2eae59e336ee684
sugyinbrs <sgnglee@gmail.com>
2022-03-11 22:57:39 +0900

    Modified: carouselSliderData 파일 수정

--- src/pages/CarouselSlider/CarouselSliderData.js
+++ src/pages/CarouselSlider/CarouselSliderData.js
@@ -1,17 +1,14 @@
 export const IMAGES = [
   {
     title: "First Slider Image",
-    description: "This is the First slider image",
     urls: "https://www.teamfresh.co.kr/resources/assets/img/%ED%99%88%ED%8E%98%EC%9D%B4%EC%A7%80_%EC%82%AC%EC%A7%84/background1.png",
   },
   {
     title: "Second Slider Image",
-    description: "This is the Second slider image",
     urls: "https://www.teamfresh.co.kr/resources/assets/img/%ED%99%88%ED%8E%98%EC%9D%B4%EC%A7%80_%EC%82%AC%EC%A7%84/background2.png",
   },
   {
     title: "Third Slider Image",
-    description: "This is the Third slider image",
     urls: "https://www.teamfresh.co.kr/resources/assets/img/%ED%99%88%ED%8E%98%EC%9D%B4%EC%A7%80_%EC%82%AC%EC%A7%84/background3.png",
   },
 ];

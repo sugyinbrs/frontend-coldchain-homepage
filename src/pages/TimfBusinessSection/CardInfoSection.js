2b83e91e5726ee9adffeb94051743acb73975373
sugyinbrs <sgnglee@gmail.com>
2022-03-13 03:07:00 +0900

    Modified: CardInfoSection CSS 수정

--- src/pages/TimfBusinessSection/CardInfoSection.js
+++ src/pages/TimfBusinessSection/CardInfoSection.js
@@ -24,14 +24,14 @@ const CardInfoContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-basis: 300px;
+  flex-basis: 420px;
   width: 25%;
   padding: 16px;
   margin: 0 auto;
 `;
 
 const CardImage = styled.img`
-  width: 300px;
+  width: 420px;
 `;
 
 const CardInfo = styled.div`
